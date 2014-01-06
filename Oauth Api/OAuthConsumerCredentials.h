ffb555320a776db82f095476c2a4141bfe9bfcfb
KaranRajpoot <karanrajpoot2007@gmail.com>
2014-01-06 16:46:40 +0530

    Update OAuthConsumerCredentials.h

--- Oauth Api/OAuthConsumerCredentials.h
+++ Oauth Api/OAuthConsumerCredentials.h
@@ -8,8 +8,8 @@
 
 //#error Replace these with your own app credentials, then remove this error.
 
-#define OAUTH_CONSUMER_KEY @"12fadeaaaeb44f0391d7962a2e59f057"
-#define OAUTH_CONSUMER_SECRET @"f8771b2117924d4bac4f01637d0892f2"
+#define OAUTH_CONSUMER_KEY @"set fitbit oauth consumer key"
+#define OAUTH_CONSUMER_SECRET @"set fitbit oauth consumer secret"
 #define getProfileInfo @"http://api.fitbit.com/1/user/-/profile.json"
 #define getActivity @"http://api.fitbit.com/1/user/-/activities/date/2013-12-27.json"
 #define getFood @"http://api.fitbit.com/1/user/-/activities/date/2013-12-27.json"
